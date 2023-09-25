7c5837523888e047f4a5dfffcbc73b7743b72662
RileyAlexis <MxRileyAlexis@gmail.com>
2023-09-25 11:13:28 -0500

    added npm packages for redux and saga

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+
+import axios from 'axios';
+
+//Components
 
 function App(props) {
+
+const getFavorites = () => {
+  axios.get('/api/favorite')
+    .then((response) => {
+      dispatch
+    })
+}
+
+  useEffect(() => {
+    console.log(cookies);
+    getFavorites();
+  }, []);
+
   return (
-    <div>
-      <h1>Giphy Search!</h1>
+    <div className="container">
+      <Header />
+    
+    <Route path="/">
+      <Search />
+    </Route>
+    <Route path="/favorites">
+      <Favorite />
+    </Route>
     </div>
+
   );
 }
 
